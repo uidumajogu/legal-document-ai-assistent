fed55c6a17594e66b2bb3e17a9b122beff613d88
uidumajogu <idumajogu@gmail.com>
2025-10-04 00:31:46 -0400

    ✨Feat(project): Initial SvelteKit project setup with TailwindCSS, ESLint, and Prettier

---
+++ src/lib/components/logo.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { cn } from '$lib/utils/cn';
+
+	import type { SVGAttributes } from 'svelte/elements';
+
+	const { class: className = '', ...otherSvgAttributes }: SVGAttributes<SVGSVGElement> = $props();
+</script>
+
+<svg
+	class={cn('h-5 w-auto fill-stone-900', className)}
+	width="630"
+	height="840"
+	viewBox="0 0 630 840"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	{...otherSvgAttributes}
+>
+	<path d="M262.5 0H0V840H630V367.5H262.5V0Z" fill="inherit" />
+	<path d="M367.5 0V262.5H630L367.5 0Z" fill="inherit" />
+</svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Logo from '$lib/components/logo.svelte';
+</script>
+
+<main class="h-screen w-screen bg-stone-100">
+	<nav class="flex h-[2.8rem] items-center justify-center border-b border-stone-300 px-8">
+		<div class="flex items-center gap-x-2">
+			<Logo />
+			<h1 class="text-xl leading-none font-light">Legal Document AI Assistant</h1>
+		</div>
+	</nav>
+
+	<section class="h-[calc(100%-2.8rem)] w-full overflow-y-auto pt-8">
+		<div class="mx-auto w-full max-w-[1440px]"></div>
+	</section>
+</main>
